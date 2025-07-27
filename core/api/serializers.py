d27f9b2d9abb2bfda2750169488d3eb7a75c62e0
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-07-27 22:50:35 +0300

    Update4

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -15,3 +15,32 @@ class ChefSerializer(serializers.ModelSerializer):
         model = Chef
         fields = "__all__"
 
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = "__all__"
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = "__all__"
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = "__all__"
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = "__all__"
+
+class TestimonialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Testimonial
+        fields = "__all__"
+
+class BlogCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogCategory
+        fields = "__all__"
