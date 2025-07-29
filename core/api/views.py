0a39659ba276f417a12155fbee03323ec8296cab
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-07-29 11:37:23 +0300

    Update 6

--- core/api/views.py
+++ core/api/views.py
@@ -9,4 +9,28 @@ from core.api.serializers import (
     ReservationSerializer, TestimonialSerializer, BlogCategorySerializer, BlogSerializer, BlogCommentSerializer,
     GalleryCategorySerializer, GallerySerializer, ContactFormSerializer, SiteSettingsSerializer
 )
+from rest_framework.generics import ListAPIView
 
+class BannerImageListAPIView(ListAPIView):
+    queryset = BannerImage.objects.all()
+    serializer_class = BannerImageSerializer
+
+class ChefListAPIView(ListAPIView):
+    queryset = Chef.objects.all()
+    serializer_class = ChefSerializer
+
+class MenuCategoryListAPIView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
+
+class MenuListAPIView(ListAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class EventListAPIView(ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class ReservationListAPIView(ListAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = EventSerializer
